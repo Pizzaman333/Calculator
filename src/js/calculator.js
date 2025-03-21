4f9dab2bfe9f09bbacb99aa9c277976ede5e5c5d
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-03-22 00:52:40 +0200

    fix clear

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,19 +6,21 @@ const equals = document.querySelector(".equals");
 const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
 const outputs = document.querySelectorAll(".calculator__output");
-
-outputs.forEach((output) =>
-  output.addEventListener(
-    "input",
-    (event) => (output.textContent = event.target.value)
-  )
-);
+const change = document.querySelector(".changeSign")
 
 operators.forEach((operator) =>
   operator.addEventListener("click", (event) => {
     outputs[1].textContent = event.target.textContent;
   })
 );
+
+change.addEventListener("click", event => {
+    if (outputs[1].textContent !== "" && outputs[2].textContent !== "") {
+        outputs[2].textContent = Number(outputs[2].textContent) * -1;
+    } else if (outputs[0].textContent !== "") {
+        outputs[0].textContent = Number(outputs[0].textContent) * -1;
+    } 
+})
 
 numbers.forEach((number) =>
   number.addEventListener("click", (event) => {
@@ -33,7 +35,6 @@ numbers.forEach((number) =>
 clear.addEventListener("click", (event) => {
   outputs.forEach((output) => {
     output.textContent = "";
-    output.value = "";
   });
   output.textContent = "";
 });
